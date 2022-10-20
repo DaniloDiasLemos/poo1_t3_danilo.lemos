cd04a3676401d07ca7d5f7f889ef460581038e4f
DaniloDiasLemos <danilo-34@hotmail.com>
2022-10-20 15:29:37 -0300

    Ex2

--- LAB01 - Procedural/ex02/exemplo4.java
+++ LAB01 - Procedural/ex02/exemplo4.java
@@ -3,43 +3,56 @@
  * ex4
  */
 import java.util.Scanner;
-import Second.alteraPreco;
+
+import produto.Produto;
 
 public class exemplo4 {
     public static void main(String[] args) {
 
-        String nome[] = new String[4];
-        float preco[] = new float[4];
-        int qtd_estoque[] = new int[4];
         Scanner sc = new Scanner(System.in);
+        Produto p1 = new Produto();
+        Produto p2 = new Produto();
+        Produto p3 = new Produto();
+        Produto p4 = new Produto();
 
-        alteraPreco alteraPreco = new alteraPreco();
+        System.out.println("Informe o nome, o preco e a qtd. em estoque do produto 1: ");
+        p1.nome = sc.next();
+        p1.preco = sc.nextFloat();
+        p1.qtd_estoque = sc.nextInt();
+        System.out.println("Informe o nome, o preco e a qtd. em estoque do produto 2: ");
+        p2.nome = sc.next();
+        p2.preco = sc.nextFloat();
+        p2.qtd_estoque = sc.nextInt();
+        System.out.println("Informe o nome, o preco e a qtd. em estoque do produto 3: ");
+        p3.nome = sc.next();
+        p3.preco = sc.nextFloat();
+        p3.qtd_estoque = sc.nextInt();
+        System.out.println("Informe o nome, o preco e a qtd. em estoque do produto 4: ");
+        p4.nome = sc.next();
+        p4.preco = sc.nextFloat();
+        p4.qtd_estoque = sc.nextInt();
 
-        for (int i = 0; i < 4; i++) {
-            System.out.println("Informe o nome, o preco e a qtd. em estoque do produto " + (i+1) +": ");
-            nome[i] = sc.nextLine();
-            preco[i] = sc.nextFloat();
-            qtd_estoque[i] = sc.nextInt();
-        }
+        sc.close();
 
         System.out.println("\nAumentando o preco em 10% do produto 1 e 3");
-        alteraPreco.altera_preco(preco[0], -110);
-        alteraPreco.altera_preco(preco[2], 10);
+        p1.altera_preco(-110);
+        p3.altera_preco(10);
 
-        System.out.println("\nReduzindo o preco em 5%% do produto 2");
-        if (alteraPreco.altera_preco(preco[1], -5) == -1)
+        System.out.println("\nReduzindo o preco em 5% do produto 2");
+        if (p2.altera_preco(-5) == -1)
             System.out.println("\n\nErro: preco nao alterado. Porcentagem invalida\n");
         else
-            System.out.println("\n\n Preco alterado com sucesso");
+            System.out.println("\n\n Preco alterado com sucesso\n");
 
         System.out.println("\nAlterando o preco do prod. 3");
-        if (alteraPreco.altera_preco(preco[3], -110) == -1)
+        if (p3.altera_preco(-110) == -1)
             System.out.println("\n\nErro: preco nao alterado. Porcentagem invalida\n");
 
         System.out.println("\nProdutos Cadastrados:\n");
-        for (int i = 0; i < 4; i++) {
-            System.out.println("\nProduto: " + nome[i] + "\nPreco: " + preco[i] + "\nEstoque: " + qtd_estoque[i]);
-        }
+        System.out.println("\nProduto: " + p1.nome + "\nPreco: "+ p1.preco + "\nEstoque: " + p1.qtd_estoque);
+        System.out.println("\nProduto: " + p2.nome + "\nPreco: "+ p2.preco + "\nEstoque: " + p2.qtd_estoque);
+        System.out.println("\nProduto: " + p3.nome + "\nPreco: "+ p3.preco + "\nEstoque: " + p3.qtd_estoque);
+        System.out.println("\nProduto: " + p4.nome + "\nPreco: "+ p4.preco + "\nEstoque: " + p4.qtd_estoque);
 
     }
 }
